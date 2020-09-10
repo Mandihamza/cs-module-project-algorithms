24fb6b1889ff5be0a29c7e31e7700cf981ec754d
mandihamza <mandihamza@gmail.com>
2020-09-09 18:34:09 -0600

    File cleanup

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,14 +1,16 @@
-'''
+"""
 Input: a List of integers
 Returns: a List of integers
-'''
+"""
+
+
 def moving_zeroes(arr):
     # Your code here
 
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
